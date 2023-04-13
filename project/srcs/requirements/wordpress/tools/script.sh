ec68bd12c76730cb3a131593c75e5819b4a823df
ooumlil <ooumlil@student.1337.ma>
2023-04-13 09:17:12 +0000

    minor update in mandatory part: wordpress script.

--- project/srcs/requirements/wordpress/tools/script.sh
+++ project/srcs/requirements/wordpress/tools/script.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-chmod +x wp-cli.phar
-
-mv wp-cli.phar /usr/local/bin/wp
-
+# Check if wp-config.php file exists
 if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+	# Download WordPress
 	wp core download --path="/var/www/html"\
 					--allow-root
+
+	# Create wp-config.php file
 	wp config create --dbname=$DATABASE_NAME\
 					--dbuser=$MYSQL_USER\
 					--dbpass=$MYSQL_PASSWORD\
 					--dbhost=mariadb\
 					--path="/var/www/html"\
 					--allow-root
+
+	# Install WordPress
 	wp core install --url=$DOMAIN_NAME/\
 					--title="Home Page"\
 					--admin_user=$MYSQL_USER\
@@ -23,21 +24,28 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 					--skip-email\
 					--path="/var/www/html"\
 					--allow-root
+
+	# Create a new user
 	wp user create "eclipse" "chiahaja@gmail.com"\
 					--role="author"\
 					--user_pass="1234"\
 					--path="/var/www/html"\
 					--allow-root
+
+	# Install and activate the Astra theme
 	wp theme install "astra"\
 					--activate\
 					--path="/var/www/html"\
 					--allow-root
 fi
 
+# Update PHP-FPM configuration to listen on port 9000
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
+# Create run/php directory if it doesn't exist
 if [ ! -d run/php ]; then
 	mkdir run/php
 fi
 
+# Start PHP-FPM
 /usr/sbin/php-fpm7.3 -F
